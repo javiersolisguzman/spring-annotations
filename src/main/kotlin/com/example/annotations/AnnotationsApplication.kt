399bb9fc8f4e163365e219d8d8560978649cd819
Javier Solis Guzman <javier.solis@creditas.com>
2021-12-13 14:21:14 -0600

    agregar controlador

--- src/main/kotlin/com/example/annotations/AnnotationsApplication.kt
+++ src/main/kotlin/com/example/annotations/AnnotationsApplication.kt
@@ -1,19 +1,28 @@
 package com.example.annotations
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class AnnotationsApplication
 
-	@Autowired
-	private lateinit var boda: Boda
+fun main(args: Array<String>) {
+  runApplication<AnnotationsApplication>(*args)
 
-	fun main(args: Array<String>) {
-		runApplication<AnnotationsApplication>(*args)
-		boda.ceremoniaPastel()
+}
 
-	}
+@RestController
+class mainController {
+
+
+  @GetMapping("/ceremonia-pastel")
+  fun main() {
+    println("yo mandaré a llamar el metodo comer pastel")
+  }
+
+
+}
 
 
