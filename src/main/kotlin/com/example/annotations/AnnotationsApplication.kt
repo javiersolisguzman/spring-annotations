77fa9b98f6d586065c664f26e341f7d5df8957e7
Javier Solis Guzman <javier.solis@creditas.com>
2021-12-13 15:08:36 -0600

    Muestra beans

--- src/main/kotlin/com/example/annotations/AnnotationsApplication.kt
+++ src/main/kotlin/com/example/annotations/AnnotationsApplication.kt
@@ -1,10 +1,14 @@
 package com.example.annotations
 
+import java.util.Arrays
+import javax.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationContext
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+
 
 @SpringBootApplication
 class AnnotationsApplication
@@ -18,6 +22,19 @@ fun main(args: Array<String>) {
 class mainController {
   @Autowired
   private lateinit var boda: Boda
+
+  @Autowired
+  private lateinit var ctx: ApplicationContext
+
+  @PostConstruct
+  fun init() {
+    println("Let's inspect the beans provided by Spring Boot:")
+    val beanNames: Array<String> = ctx.getBeanDefinitionNames()
+    Arrays.sort(beanNames)
+    for (beanName in beanNames) {
+      println(beanName)
+    }
+  }
 
   @GetMapping("/ceremonia-pastel")
   fun main() {
