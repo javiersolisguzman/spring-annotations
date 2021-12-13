5dec8afe761b72979b4e91df2739e2c20114b3f6
Javier Solis Guzman <javier.solis@creditas.com>
2021-12-13 16:40:03 -0600

    usar @Bean

--- src/main/kotlin/com/example/annotations/Invitaciones.kt
+++ src/main/kotlin/com/example/annotations/Invitaciones.kt
@@ -3,6 +3,8 @@ package com.example.annotations
 import java.util.UUID
 import javax.persistence.Entity
 import javax.persistence.Id
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
@@ -29,23 +31,47 @@ class InvitacionesController(private val invitacionesService: InvitacionesServic
 
 data class InvitacionDTO(val nombre: String)
 
-
 interface InvitacionesService {
   fun invitar(nombre: String)
   fun verInvitaciones(): List<InvitacionDTO>
 }
 
 @Service
-class InvitacionesServiceImpl(private val invitacionesRepository: InvitacionesRepository) : InvitacionesService {
+class InvitacionesServiceImpl(
+  private val invitacionesRepository: InvitacionesRepository,
+  private val mensajeriaService: MensajeriaService
+) : InvitacionesService {
 
   override fun invitar(nombre: String) {
     invitacionesRepository.save(Invitacion(nombre))
+    mensajeriaService.enviarPaqueteA(nombre)
   }
 
   override fun verInvitaciones(): List<InvitacionDTO> {
     return invitacionesRepository.findAll().map { InvitacionDTO(it.getName()) }
   }
 
+}
+
+interface MensajeriaService {
+
+  fun enviarPaqueteA(nombre: String)
+}
+
+class Fedex(val codigoCliente: String) : MensajeriaService {
+  override fun enviarPaqueteA(nombre: String) {
+    println("Soy la paqueteria fedex y pateo tu paquete")
+  }
+}
+
+
+@Configuration
+class ConfigurarMensajeria {
+
+  @Bean
+  fun configurarPaqueteria(): MensajeriaService {
+    return Fedex("123")
+  }
 }
 
 @Entity
